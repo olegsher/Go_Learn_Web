434442ef18f447055d665bdaab320f9d60f14844
Oleg Sher <olegsher@google.com>
2019-10-28 13:55:40 +0000

    test commit

--- src/SvetlanaSher/SvetlanaSher.go
+++ src/SvetlanaSher/SvetlanaSher.go
@@ -1,10 +1,10 @@
 package SvetlanaSher
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func SvetlanaSherIndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello www.svetlanasher.com!!!!") // send data to client side
+	//fmt.Fprintf(w, "Hello www.svetlanasher.com!!!!") // send data to client side
+	http.Handle("/", http.FileServer(http.Dir("./static")))
 }
